60e043fe1b27fe45b871a8d746aea39eb6bc84a0
Fathurrahman <41844599+fathuurr@users.noreply.github.com>
2023-08-22 22:03:11 +0700

    finish make portofolio website

--- lib/data.ts
+++ lib/data.ts
@@ -1,107 +1,107 @@
-import React from "react";
-import { CgWorkAlt } from "react-icons/cg";
-import { FaReact } from "react-icons/fa";
-import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import React from 'react';
+import { CgWorkAlt } from 'react-icons/cg';
+import { FaReact } from 'react-icons/fa';
+import { LuGraduationCap } from 'react-icons/lu';
+import corpcommentImg from '@/public/threads-clone.png';
+import rmtdevImg from '@/public/luxspace.png';
+import wordanalyticsImg from '@/public/topupgame.png';
 
 export const links = [
   {
-    name: "Home",
-    hash: "#home",
+    name: 'Home',
+    hash: '#home',
   },
   {
-    name: "About",
-    hash: "#about",
+    name: 'About',
+    hash: '#about',
   },
   {
-    name: "Projects",
-    hash: "#projects",
+    name: 'Projects',
+    hash: '#projects',
   },
   {
-    name: "Skills",
-    hash: "#skills",
+    name: 'Skills',
+    hash: '#skills',
   },
   {
-    name: "Experience",
-    hash: "#experience",
+    name: 'Experience',
+    hash: '#experience',
   },
   {
-    name: "Contact",
-    hash: "#contact",
+    name: 'Contact',
+    hash: '#contact',
   },
 ] as const;
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: 'Vocational High School 21 Jakarta',
+    location: 'Jakarta, Indonesia',
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+      'I graduated from a Vocational High School majoring in Software Engineering for 3 years. Since school, I have been involved in various activities, such as winning the third prize in the IT Software competition at the Central Jakarta II regional level.',
     icon: React.createElement(LuGraduationCap),
-    date: "2019",
+    date: '2018 - 2021',
   },
   {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
+    title: 'Computer and Information Technology Education',
+    location: 'Jakarta, Indonesia',
     description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
-    icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
+      'Currently, I am studying at Jakarta State of University. During my studies, I am engaged in various activities, from learning about leadership by participating in organizations such as the Student Executive Board, to keeping busy with learning about the latest technologies through creating several projects.',
+    icon: React.createElement(LuGraduationCap),
+    date: '2021 - Present',
   },
   {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
+    title: 'Frontend Engineer',
+    location: 'Jakarta, Indonesia',
     description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+      'I used to work at PT. Cakra Radha Mustika where, in this role, I learned a lot about frontend tech stack such as Next.js, Axios, Typescript, and React Formik for form validation. During those 4 months, my project was building a Custom Relationship Manager (CRM) website.',
     icon: React.createElement(FaReact),
-    date: "2021 - present",
+    date: 'March 2022 - June 2022',
   },
 ] as const;
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: 'Mention',
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
+      'This is a Mention feature, where in the application I developed, it is similar to the currently trending Meta application, Threads.',
+    tags: ['Typescript', 'Next.js 13', 'MongoDB', 'Tailwind', 'Clerk'],
     imageUrl: corpcommentImg,
   },
   {
-    title: "rmtDev",
+    title: 'Luxspace',
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
+      'The website offers elegant furniture for household needs and creates rooms that are visually pleasing.',
+    tags: ['React', 'Javascript', 'Tailwind', 'Redux', 'Axios'],
     imageUrl: rmtdevImg,
   },
   {
-    title: "Word Analytics",
+    title: 'Top Up Game Store',
     description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+      'The website provides game top-up services to facilitate the fast and reliable needs of gamers.',
+    tags: [
+      'Typescript',
+      'Next.js',
+      'MongoDB',
+      'Express.js',
+      'Tailwind',
+      'Axios',
+    ],
     imageUrl: wordanalyticsImg,
   },
 ] as const;
 
 export const skillsData = [
-  "HTML",
-  "CSS",
-  "JavaScript",
-  "TypeScript",
-  "React",
-  "Next.js",
-  "Node.js",
-  "Git",
-  "Tailwind",
-  "Prisma",
-  "MongoDB",
-  "Redux",
-  "GraphQL",
-  "Apollo",
-  "Express",
-  "PostgreSQL",
-  "Python",
-  "Django",
-  "Framer Motion",
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Next.js',
+  'Node.js',
+  'Git',
+  'Tailwind',
+  'MongoDB',
+  'Redux',
+  'Express.js',
 ] as const;
