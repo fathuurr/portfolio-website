1c0a987b18ef22f18e5282a06814ded3feb72ec9
Fathurrahman <41844599+fathuurr@users.noreply.github.com>
2023-08-22 22:16:46 +0700

    change config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "images.unsplash.com",
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
       },
     ],
   },
